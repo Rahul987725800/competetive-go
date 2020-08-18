e2781e81698f23852be1c851a58e86f937aa1560
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-19 04:07:35 +0530

    basic maths functions

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"first/maths"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("ghanta van cho")
+	fmt.Println(maths.Add(1, 4))
+	fmt.Println(maths.Max(9, 10))
+}
---
+++ maths/utils.go
@@ -0,0 +1,22 @@
+package maths
+
+func Abs(a, b int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
