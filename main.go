0276aca4e2fd139bdc5c20bf9a4e87b9d737ccf0
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-19 04:18:21 +0530

    very nice template

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"first/maths"
 	"fmt"
+	"github.com/Pallinder/go-randomdata"
+	"math"
 )
 
 func main() {
 	fmt.Println("ghanta van cho")
 	fmt.Println(maths.Add(1, 4))
 	fmt.Println(maths.Max(9, 10))
+	fmt.Println(randomdata.SillyName())
+	fmt.Println(math.MaxInt32)
 }
